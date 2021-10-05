815b97f1e95947df34468accac364f761e9a3f5e
Adityachess <adityadone9398@gmail.com>
2021-10-05 15:05:03 +0530

    ADD:Calculating the length using two points.

--- LineComparison.java
+++ LineComparison.java
@@ -1,10 +1,33 @@
 package com.bz.linecomparison;
 
+import java.util.Scanner;
+
 public class LineComparison {
 
+	static Scanner scan = new Scanner(System.in);
+
+	public void calculateLength() {
+
+		System.out.println("Enter the values:");
+
+		double x1 = scan.nextDouble();
+		double x2 = scan.nextDouble();
+		double y1 = scan.nextDouble();
+		double y2 = scan.nextDouble();
+
+		double len1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+		System.out.println("length is:" + len1);
+	}
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Welcome to Line Comparison Computation Program");
+		
+		 LineComparison object = new  LineComparison();
+		 
+		 object.calculateLength();
+		
 
 	}
 
